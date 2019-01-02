68976702565c58ab8678a04005a87cbbea05ac88
chandankumar18 <chandan.kumar1990@yahoo.com>
2019-01-02 11:06:19 +0530

    Adding one inner for loop

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,8 +3,12 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		for(int i=0; i<=10; i++) {
-			System.out.println("Hello World.... "+i);
+			for(int j=0; j<i; j++){
+			System.out.println("Chandan Kumar Malhotra.... "+j);
+		    }
+		System.out.println("Hello World.... "+i);
 		}
+		
 	}
 
 }
